1e6dc5935c385f54534cc3985138a227fb9a74a9
Simon Rose <simon.rose@ess.eu>
2025-06-16 14:52:11 +0200

    Update tab completion script

--- require-ess/tools/iocsh_complete.bash
+++ require-ess/tools/iocsh_complete.bash
@@ -1,13 +1,30 @@
-_iocsh_bash() {
-  local cur opts
+#!/usr/bin/env bash
+
+_iocsh_completion() {
+  local cur prev opts mods
   COMPREPLY=()
   cur="${COMP_WORDS[COMP_CWORD]}"
-  opts="-h -v -c -s -r -e -dg -dv -l -n"
+  prev="${COMP_WORDS[COMP_CWORD - 1]}"
 
-  if [[ ${cur} == -* ]]; then
-    mapfile -t COMPREPLY < <(compgen -W "${opts}" -- "${cur}")
+  #  -r should try to determine modules
+  if [[ ${prev} == -r ]]; then
+    if [ -n "${EPICS_MODULES}" ]; then
+      mods=$(ls "${EPICS_MODULES}/" 2>/dev/null)
+      # shellcheck disable=SC2207
+      COMPREPLY=($(compgen -W "${mods}" -- "${cur}"))
+    fi
     return 0
   fi
+
+  opts="-h -V -c -r -dg -dv"
+  if [[ ${cur} == -* ]]; then
+    # shellcheck disable=SC2207
+    COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
+    return 0
+  fi
+
+  # shellcheck disable=SC2207
+  COMPREPLY=($(compgen -f -- "${cur}"))
 }
 
-complete -o filenames -o nospace -o bashdefault -F _iocsh_bash iocsh
+complete -o filenames -o nospace -o bashdefault -F _iocsh_completion iocsh
