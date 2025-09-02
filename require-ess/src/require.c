bc7121a9d62cbfa0211315732fc0cf2c7f08d503
Lucas A. M. Magalhaes <lucas.magalhaes@ess.eu>
2025-09-02 10:00:50 +0200

    Cleanup directory macros

--- require-ess/src/require.c
+++ require-ess/src/require.c
@@ -81,25 +81,6 @@ int requireDebug;
 #define EXT ".so"
 #endif
 #include <dirent.h>
-#define DIR_HANDLE DIR *
-#define IF_OPEN_DIR(f) if ((dir = opendir(f)))
-#define DIR_ENTRY struct dirent *
-#define START_DIR_LOOP while ((errno = 0, direntry = readdir(dir)) != NULL)
-#define END_DIR_LOOP                                                           \
-  if (!direntry && errno)                                                      \
-    errlogPrintf("error reading directory %s: %s\n", filename,                 \
-                 strerror(errno));                                             \
-  if (dir)                                                                     \
-    closedir(dir);
-#ifdef _DIRENT_HAVE_D_TYPE
-#define SKIP_NON_DIR(e)                                                        \
-  if (e->d_type != DT_DIR && e->d_type != DT_UNKNOWN)                          \
-    continue;
-#else // _DIRENT_HAVE_D_TYPE
-#define SKIP_NON_DIR(e)
-#endif // _DIRENT_HAVE_D_TYPE
-#define FILENAME(e) e->d_name
-
 #define LIBDIR "lib" OSI_PATH_SEPARATOR
 #define DEPDIR "dep" OSI_PATH_SEPARATOR
 #define TEMPLATEDIR "db"
